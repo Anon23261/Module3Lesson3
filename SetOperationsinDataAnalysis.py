d1933c28cf06e799657e7821b2545869bbc65090
Anon <anonymous23261@icloud.com>
2024-10-29 14:11:02 -0600

    SetOperationsinDataAnalysis.py

---
+++ SetOperationsinDataAnalysis.py
@@ -0,0 +1,8 @@
+# Given list of customer IDs
+customer_ids = ["C001", "C002", "C003", "C002", "C001", "C004"]
+
+# Using a set to remove duplicates
+unique_customer_ids = set(customer_ids)
+
+# Displaying the unique customer IDs
+print(unique_customer_ids)
